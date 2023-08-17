436a281d560493af18882c89b2c2792a440e0781
Anwesh <anweshknayak@gmail.com>
2023-08-18 00:03:17 +0530

    fix init

--- src/main/java/com/javaguides/generics/methods/Generics.java
+++ src/main/java/com/javaguides/generics/methods/Generics.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+import java.util.stream.Collectors;
 
 public class Generics {
 
